f81794705686efa919b4fc63b14d39c34b940145
DarshanBapodariya <darshanbapodariya88@gmail.com>
2020-08-14 01:22:34 +0200

    okk

--- api_mgt.php
+++ api_mgt.php
@@ -1,3 +1,4 @@
+<!-- <html>
 <script>
 function displayCoronaTable(){
     
@@ -14,62 +15,61 @@ function displayCoronaTable(){
        var cell_data = covid_data[count].split(",");
       
        table_data += '<tr>';
+      
        
-
        
-       
-       for(var cell_count=0; cell_count<cell_data.length; cell_count++)
-       {
-          if(cell_count <= 5 )
-          {
-        if(count === 0)
-        {
+      //  for(var cell_count=0; cell_count<cell_data.length; cell_count++)
+      //  {
+      //     if(cell_count <= 5 )
+      //     {
+      //   if(count === 0)
+      //   {
             
-         table_data += '<th>'+cell_data[cell_count]+'</th>';
+      //    table_data += '<th>'+cell_data[cell_count]+'</th>';
             
-        }
-        else if(count != 1)
-        {
+      //   }
+      //   else if(count != 1)
+      //   {
 
-            <?php include('connection.php');
 
-            $state=cell_data[0];
-            $cnf=cell_data[1];
-            $rec=cell_data[2];
-            $death=cell_data[3];
-            $act=cell_data[4];
-            $date=cell_data[5];
+      //        var val = cell_data[cell_count];
 
-             $sqlQuery="INSERT INTO ind_dashboard (`state`, `cnf`, `rec`, `death`, `act`, `date`) VALUES (`$state`, `$cnf`, `$rec`, `$death`, `$act`, `$date`)";
-             $result=mysqli_query($db,$sqlQuery);  
+      //       if(val.length <= 45 && val != '"')
+      //       {
+      //           table_data += '<td>'+cell_data[cell_count]+'</td>';
+      //       }
+      //   }
+      // }
+      //  }
+      //  table_data += '</tr>';
+      // }
 
-            if($result)
-			echo " </br> The Hospital data added successfully  !  ";
-		    else
-			echo " <br/> The Hospital data not added successfully  !  ";
-            ?>
-
-             var val = cell_data[cell_count];
-
-            if(val.length <= 45 && val != '"')
-            {
-                table_data += '<td>'+cell_data[cell_count]+'</td>';
-            }
-        }
-      }
-       }
-       table_data += '</tr>';
-      }
-
-      table_data += '</table>';
+      // table_data += '</table>';
      
-      //$('#covid_table').html(table_data);
+      // //$('#covid_table').html(table_data);
 
      }
     });
-   
-   
   }
-
 displayCoronaTable();
 </script>
+</html> -->
+
+<?php include('connection.php');
+
+  if(isset($_post[state])&&isset($_post[cnf])&&isset($_post[rec])&&isset($_post[death])&&isset($_post[act]))
+  $state="<script>cell_data[0]</script>";
+  $cnf="<script>cell_data[1]</script>";
+  $rec="<script>cell_data[2]</script>";
+  $death="<script>cell_data[3]</script>";
+  $act="<script>cell_data[4]</script>";
+  $date="<script>cell_data[5]</script>";
+
+  $sqlQuery="INSERT INTO ind_dashboard (`state`, `cnf`, `rec`, `death`, `act`) VALUES (0, 0, 0, 0, 0)";
+  $result=mysqli_query($db,$sqlQuery);  
+
+  if($result)
+    echo " </br> The Hospital data added successfully  !  ";
+  else
+    echo " <br/> The Hospital data not added successfully  !  ";
+?>
